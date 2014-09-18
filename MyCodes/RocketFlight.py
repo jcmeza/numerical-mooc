b927537ee8bbcb0e44fa172be5e5b5b2a2a6371b
Juan C. Meza <jcmeza@ucmerced.edu>
2014-09-17 17:51:10 -0700

    Fixed bug
    Forgot to divide by total mass in second equation, third term

--- MyCodes/RocketFlight.py
+++ MyCodes/RocketFlight.py
@@ -1,19 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 16 16:37:28 2014
-
-@author: jcmeza
-"""
-
-from math import sin, cos, log, ceil
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
+#matplotlib inline
 from matplotlib import rcParams
 rcParams['font.family'] = 'serif'
 rcParams['font.size'] = 16
 # model parameters
-m_s   50     # weight of the rocket, kg
+m_s =  50     # weight of the rocket, kg
 g = 9.8      # gravity in m s^{-2} 
 rho = 1.091  # average air density, kg/m^3
 r   = 0.5    # radius of rocket
@@ -23,7 +15,7 @@ C_D = 0.15   # drag coefficient --- or D/L if C_L=1
 m_p0 = 100   # initial weight of rocket propellant, kg
 
 ### set initial conditions ###
-v0 = 0.     # start at the trim velocity (or add a delta)
+v0 = 0.     # start at standstill
 h0 = 0.0    # initial altitude
 
 def f(u,tn):
@@ -42,15 +34,16 @@ def f(u,tn):
     
     h   = u[0]
     v   = u[1]
-    m_p = np.max([m_p0. - 20.*tn,0])
+    m_p = np.max([m_p0 - 20.*tn,0])
+    total_mass = m_s + m_p
     
     if tn <= 5.0:
         mpdot = 20.
     else:
         mpdot = 0.
         
-    return numpy.array([v,
-                      -g + v_e/(m_s+m_p)*mpdot - 0.5*rho*v*np.abs(v)*A*C_d,
+    return np.array([v,
+        -g + v_e/(total_mass*mpdot - (0.5*rho*v*np.abs(v)*A*C_D)/(total_mass),
                       ])
                       
 def euler_step(u, f, dt, n):
@@ -77,13 +70,14 @@ def euler_step(u, f, dt, n):
 T  = 10.0                          # final time
 dt = 0.1                           # time increment
 N  = int(T/dt) + 1                  # number of time-steps
-t  = numpy.linspace(0.0, T, N)      # time discretization
+t  = np.linspace(0.0, T, N)      # time discretization
 
 # initialize the array containing the solution for each time-step
-u = numpy.empty((N, 2))
-u[0] = numpy.array([h0, v0])# fill 1st element with initial values
+u = np.empty((N, 2))
+u[0] = np.array([h0, v0])# fill 1st element with initial values
 
 # time loop - Euler method
 for n in range(N-1):
     
     u[n+1] = euler_step(u[n], f, dt, n)
+
